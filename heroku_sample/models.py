ab26f6e319bfdce139161cd5bf657dad3586133d
Benson P Sabu <benzps01@gmail.com>
2023-08-30 22:52:29 +0530

    minor changes

--- heroku_sample/models.py
+++ heroku_sample/models.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 print("This is the database_url", os.environ["DATABASE_URL"])
-# database_path = os.environ["DATABASE_URL"]
-database_path = "postgresql://postgres@127.0.0.1:5432/postgres"
+database_path = os.environ["DATABASE_URL"]
+# database_path = "postgresql://postgres@127.0.0.1:5432/postgres"
 if database_path.startswith("postgres://"):
     database_path = database_path.replace("postgres://", "postgresql://", 1)
 
